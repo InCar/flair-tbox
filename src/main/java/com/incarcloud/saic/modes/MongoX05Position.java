3778338cc32a195143eb28e12d0cc9714f5cf257
hushen <13164159381@163.com>
2018-08-21 16:27:44 +0800

    补充(position转译数据)

--- src/main/java/com/incarcloud/saic/modes/MongoX05Position.java
+++ src/main/java/com/incarcloud/saic/modes/MongoX05Position.java
@@ -14,18 +14,21 @@ public class MongoX05Position extends MongoX implements IMongoX05Position {
         String vin = super.getVin(bsonDoc);
         ZonedDateTime tmGMT8 = super.getZonedDateTimeGMT8(bsonDoc);
         int gpsStatus = Integer.parseInt(bsonDoc.getString("gpsStatus"));
-        double gnssLat = Integer.parseInt(bsonDoc.getString("gnssLat"));
-        double gnssLong = Integer.parseInt(bsonDoc.getString("gnssLong"));
+        double gnssLat = Double.parseDouble(bsonDoc.getString("gnssLat"));
+        double gnssLong = Double.parseDouble(bsonDoc.getString("gnssLong"));
 
         GBx05Position gBx05Position = new GBx05Position(vin, tmGMT8);
+        gBx05Position.setPositionStatus(calcGpsStatus(gpsStatus));
+        gBx05Position.setLatitude(calcGnssLong(gnssLat));
+        gBx05Position.setLongitude(calcGnssLat(gnssLong));
         return gBx05Position;
     }
 
-    private static Integer calcGpsStatus(int gpsStatus){
+    private static byte calcGpsStatus(int gpsStatus){
         /*IF gpsStatus=0
         THEN 定位状态=1
         ELSE 定位状态=0*/
-        Integer GS;
+        byte GS;
         if (gpsStatus == 0) GS = 1;
         else GS = 0;
         return GS;
@@ -33,14 +36,12 @@ public class MongoX05Position extends MongoX implements IMongoX05Position {
 
     private static Double calcGnssLat(double gnssLat){
         //经度,精确到百万分之一度。
-        Double GL = mul(gnssLat,0.000001d);
-        return GL;
+        return gnssLat;
     }
 
     private static Double calcGnssLong(double gnssLong){
         //纬度,精确到百万分之一度。
-        Double GL = mul(gnssLong,0.000001d);
-        return GL;
+        return gnssLong;
     }
 
 
