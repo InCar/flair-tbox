328e5389899eee85e21e10d8f537922cdcf0e8bc
xiaoyao <xy402506559@qq.com>
2018-08-22 14:28:18 +0800

    ip32p

---
+++ src/main/java/com/incarcloud/saic/modes/IP32P/IP32Px01Overview.java
@@ -0,0 +1,11 @@
+package com.incarcloud.saic.modes.IP32P;
+
+import com.incarcloud.saic.modes.MongoX01Overview;
+
+/**
+ * ip32p = ip34 = as24
+ * @author xy
+ */
+public class IP32Px01Overview extends MongoX01Overview {
+
+}
