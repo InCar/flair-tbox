39ec903b25badce0a9cfbe5a2d5b785f50f87c7d
wanghan <342974886@qq.com>
2018-08-24 15:50:32 +0800

    Peak修改

--- src/main/java/com/incarcloud/saic/modes/AS26/AS26x06Peak.java
+++ src/main/java/com/incarcloud/saic/modes/AS26/AS26x06Peak.java
@@ -20,7 +20,8 @@ public class AS26x06Peak extends MongoX implements IMongoX06Peak  {
 
         GBx06Peak data = new GBx06Peak(vin, tmGMT8);
         //最高电压电池子系统号=0x01
-        data.setHighBatteryId((byte) 0x01);
+        data.setHighBatteryId((short) 0x01);
+        @SuppressWarnings("unchecked")
         /*电池单体电压最高值=max[vehBMSCellVolt(1) ,…, vehBMSCellVolt(单体电池总数)]*/
         List<Document> tmplist = (List<Document>)bsonDoc.get("vehBMSCellVolt", List.class);
         float vehBMSCellMaxVol = 0f;
@@ -32,18 +33,18 @@ public class AS26x06Peak extends MongoX implements IMongoX06Peak  {
                 }
             }
         }
-        data.setHighVoltage(vehBMSCellMaxVol*0.001f);
+        data.setHighVoltage(vehBMSCellMaxVol);
         /*IF电池单体电压最高值=65.535
         THEN最高电压电池单体代号=0xFF
         ELSE 最高电压电池单体代号=Index{电池单体电压最高值 of [vehBMSCellVolt(1) ,…, vehBMSCellVolt(单体电池总数)]}*/
         float highVoltage = data.getHighVoltage();
-        byte indexVol= 0;
+        short indexVol= 0;
         if(highVoltage == 65.535){
-            data.setHighBatteryCode((byte)0xFF);
+            data.setHighBatteryCode((short) 0xFF);
         }else{
             List<Document> tmplist1 = (List<Document>)bsonDoc.get("vehBMSCellVolt", List.class);
             if(tmplist1 != null){
-                for (byte i = 0; i < tmplist1.size(); i++) {
+                for (short i = 0; i < tmplist1.size(); i++) {
                     Document subDoc = tmplist1.get(i);
                     if (highVoltage ==  Float.parseFloat(subDoc.getString("vehBMSCellVolt"))) {
                         indexVol = i ;
@@ -53,9 +54,9 @@ public class AS26x06Peak extends MongoX implements IMongoX06Peak  {
             }
          }
 
-        data.setHighBatteryCode((byte) (indexVol+1));
+        data.setHighBatteryCode((short) (indexVol+1));
         //最低电压电池子系统号
-        data.setLowBatteryId((byte) 0x1);
+        data.setLowBatteryId((short) 0x1);
         //电池单体电压最低值=min[vehBMSCellVolt(1) ,…, vehBMSCellVolt(单体电池总数)]
         List<Document> tmpEminlist = (List<Document>)bsonDoc.get("vehBMSCellVolt", List.class);
         float vehBMSCellVoltMin = Float.MAX_VALUE;
@@ -67,18 +68,18 @@ public class AS26x06Peak extends MongoX implements IMongoX06Peak  {
                 }
             }
         }
-        data.setLowVoltage(vehBMSCellVoltMin*0.001f);
+        data.setLowVoltage(vehBMSCellVoltMin);
         /*IF电池单体电压最低值=65.535
         THEN最低电压电池单体代号=0xFF
         ELSE 最低电压电池单体代号=Index{电池单体电压最低值 of [vehBMSCellVolt(1) ,…, vehBMSCellVolt(单体电池总数)]}*/
         float minVoltage = data.getLowVoltage();
-        byte indexVolMin= 0;
+        short indexVolMin= 0;
         if(minVoltage == 65.535){
-            data.setLowBatteryCode((byte)0xFF);
+            data.setLowBatteryCode((short)0xFF);
         }else{
             List<Document> tmplistmin = (List<Document>)bsonDoc.get("vehBMSCellVolt", List.class);
             if(tmplistmin != null){
-                for (byte i = 0; i < tmplistmin.size(); i++) {
+                for (short i = 0; i < tmplistmin.size(); i++) {
                     Document subDoc = tmplistmin.get(i);
                     if (minVoltage ==  Float.parseFloat(subDoc.getString("vehBMSCellVolt"))) {
                         indexVolMin = i ;
@@ -89,73 +90,75 @@ public class AS26x06Peak extends MongoX implements IMongoX06Peak  {
         }
         data.setLowBatteryCode(indexVolMin);
         //最高温度子系统号
-        data.setHighTemperatureId((byte) 0x1);
+        data.setHighTemperatureId((short) 0x1);
         //最高温度值=max[vehBMSCellTem(1) ,…, vehBMSCellTem(可充电储能温度探针个数)]
         List<Document> tmpTemperaturelist = (List<Document>)bsonDoc.get("vehBMSCellTem", List.class);
-        byte vehBMSCellTemMax = 0;
+        short vehBMSCellTemMax = 0;
         if(tmpTemperaturelist != null){
             for(Document subDoc : tmpTemperaturelist){
-                byte vol = Byte.parseByte(subDoc.getString("vehBMSCellTem"));
+                short vol = Short.parseShort(subDoc.getString("vehBMSCellTem"));
                 if (vol > vehBMSCellTemMax) {
                     vehBMSCellTemMax = vol;
                 }
             }
         }
-        data.setHighTemperature((byte) (vehBMSCellTemMax-40));
+        data.setHighTemperature((vehBMSCellTemMax));
         /*IF最高温度值=215
         THEN最高温度探针序号=0xFF
         ELSE 最高温度探针序号=Index{最高温度值 of [vehBMSCellTem(1) ,…, vehBMSCellTem(可充电储能温度探针个数)]}*/
-        byte highTemperatureTmp = data.getHighTemperature();
-        byte indexTemperature= 0;
-        if((highTemperatureTmp & 0xff) == 215){
-            data.setHighProbeCode((byte)0xFF);
+
+        short highTemperatureTmp = data.getHighTemperature();
+        short indexTemperature= 0;
+        if(highTemperatureTmp  == 215){
+            data.setHighProbeCode((short)0xFF);
         }else{
             List<Document> tmplistHighTem = (List<Document>)bsonDoc.get("vehBMSCellTem", List.class);
             if(tmplistHighTem != null){
-                for (byte i = 0; i < tmplistHighTem.size(); i++) {
+                for (short i = 0; i < tmplistHighTem.size(); i++) {
                     Document subDoc = tmplistHighTem.get(i);
-                    if (highTemperatureTmp ==  Byte.parseByte(subDoc.getString("vehBMSCellTem"))) {
+                    if (highTemperatureTmp ==  Short.parseShort(subDoc.getString("vehBMSCellTem"))) {
                         indexTemperature = i ;
                         break;
                     }
                 }
             }
         }
-        data.setHighProbeCode((byte) (indexTemperature+1));
+        data.setHighProbeCode((short) (indexTemperature+1));
         //最低温度子系统号
-        data.setLowTemperatureId((byte) 0x1);
+        data.setLowTemperatureId((short) 0x1);
 
         /*最低温度值=min[vehBMSCellTem(1) ,…, vehBMSCellTem(可充电储能温度探针个数)]*/
         List<Document> tmplistLowTem = (List<Document>)bsonDoc.get("vehBMSCellTem", List.class);
-        byte vehBMSCellTemMin = Byte.MAX_VALUE;
+        short vehBMSCellTemMin = Short.MAX_VALUE;
         if(tmplistLowTem != null){
             for(Document subDoc : tmplistLowTem){
-                byte vol = Byte.parseByte(subDoc.getString("vehBMSCellTem"));
+                short vol = Short.parseShort(subDoc.getString("vehBMSCellTem"));
                 if (vol < vehBMSCellTemMin) {
                     vehBMSCellTemMin = vol;
                 }
             }
         }
 
-        data.setLowTemperature((byte) (vehBMSCellTemMin - 40));
+        data.setLowTemperature(vehBMSCellTemMin );
         /*  IF最低温度值=215
         THEN最低温度探针序号=0xFF
         ELSE 最低温度探针序号=Index{最低温度值 of [vehBMSCellTem(1) ,…, vehBMSCellTem(可充电储能温度探针个数)]}*/
         List<Document> tmplistLowTem1 = (List<Document>)bsonDoc.get("vehBMSCellTem", List.class);
-        byte lowTemperatureTmp = data.getLowTemperature();
-        byte indexTemperatureMin= 0;
-        if((lowTemperatureTmp & 0xff) == 215){
-            data.setLowProbeCode((byte)0xFF);
+
+        short lowTemperatureTmp = 0; // data.getLowTemperature();
+        short indexTemperatureMin= 0;
+        if((lowTemperatureTmp  ) == 215){
+            data.setLowProbeCode((short)0xFF);
         }else{
-            for (byte i = 0; i < tmplistLowTem1.size(); i++) {
+            for (short i = 0; i < tmplistLowTem1.size(); i++) {
                 Document subDoc = tmplistLowTem1.get(i);
-                if (lowTemperatureTmp ==  Byte.parseByte(subDoc.getString("vehBMSCellTem"))) {
+                if (lowTemperatureTmp ==  Short.parseShort(subDoc.getString("vehBMSCellTem"))) {
                     indexTemperatureMin = i ;
                     break;
                 }
             }
         }
-        data.setLowProbeCode((byte) (indexTemperatureMin+1));
+        data.setLowProbeCode((short) (indexTemperatureMin+1));
 
         return data;
     }
--- src/main/java/com/incarcloud/saic/modes/OLD_AS24/OLD_AS24x06Peak.java
+++ src/main/java/com/incarcloud/saic/modes/OLD_AS24/OLD_AS24x06Peak.java
@@ -16,6 +16,34 @@ public class OLD_AS24x06Peak extends MongoX implements IMongoX06Peak {
         String vin = super.getVin(bsonDoc);
         ZonedDateTime tmGMT8 = super.getZonedDateTimeGMT8(bsonDoc);
         GBx06Peak data = new GBx06Peak(vin, tmGMT8);
+        //最高电压电池子系统号=0x01
+        data.setHighBatteryId((short) 0x01);
+        // 最 高 电 压 电 池 单 体 代 号=0xFF
+        data.setHighBatteryCode((short) 0xFF);
+        //电 池 单 体 电 压 最 高 值 = vehBMSCellMaxVol
+        float vehBMSCellMaxVol = Float.parseFloat(bsonDoc.getString("vehBMSCellMaxVol"));
+        data.setHighVoltage(vehBMSCellMaxVol);
+        //最低电压电池子系统号=0x1
+        data.setLowBatteryId((short) 0x1);
+        //最 高低电 压 电 池 单 体 代 号=0xFF
+        data.setLowBatteryCode((short) 0xFF);
+        //电池单体电压最低值=vehBMSCellMinVol
+        float vehBMSCellMinVol = Float.parseFloat(bsonDoc.getString("vehBMSCellMinVol"));
+        data.setLowVoltage(vehBMSCellMinVol);
+        //最高温度子系统号=0x1
+        data.setHighTemperatureId((short) 0x1);
+        //最 高 温 度 探 针 序 号=0xFF
+        data.setHighProbeCode((short) 0xFF);
+        //最高温度值=vehBMSCellMaxTem
+        short vehBMSCellMaxTem = Short.parseShort(bsonDoc.getString("vehBMSCellMaxTem"));
+        data.setHighTemperature(vehBMSCellMaxTem);
+        //最低温度子系统号=0x1
+        data.setLowTemperatureId((short) 0x1);
+        //最 高 温 度 探 针 序 号 =0xFF
+        data.setLowProbeCode((short) 0xFF);
+        //最低温度值=vehBMSCellMinTem
+        short vehBMSCellMinTem = Short.parseShort(bsonDoc.getString("vehBMSCellMinTem"));
+        data.setLowTemperature(vehBMSCellMinTem);
         return data;
     }
 }
