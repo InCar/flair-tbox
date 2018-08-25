7a1071489bb961ba0c4e3c92b2b95efe1c8b7a87
wanghan <342974886@qq.com>
2018-08-25 11:16:27 +0800

    0x06Peak

---
+++ src/main/java/com/incarcloud/saic/modes/OLD_BP34/OLD_BP34x06Peak.java
@@ -0,0 +1,8 @@
+package com.incarcloud.saic.modes.OLD_BP34;
+
+/**
+ * @author wanghan
+ * @date 2018/8/25 11:06
+ */
+public class OLD_BP34x06Peak {
+}
---
+++ src/main/java/com/incarcloud/saic/modes/OLD_IP24MCE/OLD_IP24MCEx06Peak.java
@@ -0,0 +1,8 @@
+package com.incarcloud.saic.modes.OLD_IP24MCE;
+
+/**
+ * @author wanghan
+ * @date 2018/8/24 16:06
+ */
+public class OLD_IP24MCEx06Peak {
+}
