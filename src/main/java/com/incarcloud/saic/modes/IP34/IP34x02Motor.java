9cfa9041caf65f8eb8d363922dfcbe8748a4fa24
孙俊 <strange001@163.com>
2018-08-29 10:27:57 +0800

    BUG修复

--- src/main/java/com/incarcloud/saic/modes/IP34/IP34x02Motor.java
+++ src/main/java/com/incarcloud/saic/modes/IP34/IP34x02Motor.java
@@ -89,7 +89,7 @@ public class IP34x02Motor extends MongoX implements IMongoX02Motor {
         }
 
         // 驱动电机温度
-        m.setMotorTemperature((short) Integer.parseInt(prefix + "SttrTem"));
+        m.setMotorTemperature((short) Integer.parseInt(bsonDoc.getString(prefix + "SttrTem")));
 
         // 电机控制器输入电压
         float volt = Float.parseFloat(bsonDoc.getString("vehHVDCDCHVSideVolV"));
