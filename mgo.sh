904d9a7eb7531ed2781b47667dbd86d9db08e7b6
XGH <ghxiong@incarcloud.com>
2018-09-11 00:38:10 +0800

    oss op

--- mgo.sh
+++ mgo.sh
@@ -4,7 +4,9 @@
 readonly strBegin="2017-09-01"
 readonly strEnd="2017-09-03"
 
+readonly oss="oss://ic-saic"
 readonly sourceDir="/saic/data"
+readonly outDir="/saic/output"
 readonly db="newrvm"
 readonly prefixC="newrvmSignalData"
 
@@ -14,27 +16,38 @@ process(){
     tmYMD=$(date -d @$1 +%Y%m%d)
     tmYMD2=$(date -d @$1 +%F)
 
-    # step 1 pigz -> tar -> mongorestore
     echo $(date +"%F %T") - processing $tmYMD2 ...
     fileTGZ=$sourceDir/${tmYMD}.tgz
     fileBSON=app/dump/$tmYMD/$db/${prefixC}${tmYMD}.bson
 
+    echo $(date +"%F %T") - processing $tmYMD2 ...
+
+    # step 1 oss copy
+    ossutil64 cp $oss/saic2017mgo/${tmYMD}.tgz $fileTGZ
+
+    # step 2 pigz -> tar -> mongorestore
     pigz -p 2 -dc $fileTGZ | tar -xOf - $fileBSON | mongorestore -d $db -c ${prefixC}${tmYMD} -j 16 --noIndexRestore -vv -
 
-    # step 2 mongodb create index
+    # step 3 mongodb create index
     echo $(date +"%F %T") - create index ...
     mongo $db --eval "db.${prefixC}${tmYMD}.createIndex({vin:\"hashed\"}, {background:false})"
 
-    # step 3 java
+    # step 4 java
     echo $(date +"%F %T") - transforming ...
     java -jar saic-2017-1.0.0.jar --spring.profiles.active=product \
                                   --saic2017.beginDate="$tmYMD2" --saic2017.endDate="$tmYMD2" \
                                   --saic2017.mongo.database="$db" --saic2017.mongo.collection="${prefixC}${tmYMD}" \
+                                  --saic2017.out="$outDir" \
          > /dev/null 2>&1
 
-    # step 4 mongodb drop
+    # step 5 oss copy back
+    fileGZ=$(date -d @$1 +%Y)/$(date -d @$1 +%m)/${tmYMD}.tar.gz
+    ossutil64 cp $outDir/$fileGZ ${oss}-output/mgo/$fileGZ
+
+    # step 6 clear
     echo $(date +"%F %T") - clear ...
     mongo $db --eval "db.${prefixC}${tmYMD}.drop()"
+    rm -f $fileTGZ
 }
 
 ##############################
