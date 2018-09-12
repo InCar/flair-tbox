ccc8a67a86e76e4a6070c3dac1912c081226e1bd
Guo <gkbattle@163.com>
2018-09-12 16:04:59 +0800

    IP32 02

---
+++ src/main/java/com/incarcloud/saic/modes/IP32P/IP32Px02Motor.java
@@ -0,0 +1,11 @@
+package com.incarcloud.saic.modes.IP32P;
+
+import com.incarcloud.saic.modes.IP34.IP34x02Motor;
+
+/**
+ * @author GuoKun
+ * @version 1.0
+ * @create_date 2018/9/12 16:03
+ */
+public class IP32Px02Motor extends IP34x02Motor {
+}
